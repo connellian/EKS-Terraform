a52c303715ff1a1db5312a87c118bb01eb3466f3
Ian <connellian@yahoo.com>
2023-03-31 14:24:29 -0400

    commented out the s3 resrouce, since the bucket already exists

--- cicd-s3.tf
+++ cicd-s3.tf
@@ -1,9 +1,9 @@
-resource "aws_s3_bucket" "codepipeline_artifacts" {
-  bucket = "aws-eks-terraform-demo-create"
-} 
+#resource "aws_s3_bucket" "codepipeline_artifacts" {
+ # bucket = "aws-eks-terraform-demo-create"
+#} 
 
 
-resource "aws_s3_bucket_acl" "codepipeline_artifacts_acl" {
-  bucket = aws_s3_bucket.codepipeline_artifacts.id
-  acl    = "private"
-}
+#resource "aws_s3_bucket_acl" "codepipeline_artifacts_acl" {
+#  bucket = aws_s3_bucket.codepipeline_artifacts.id
+#  acl    = "private"
+#}
